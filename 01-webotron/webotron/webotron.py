1ebd5e7745182334b5ccb3e002a40afd2d51d2dc
saima <saima.khan@geminisolutions.in>
2020-07-15 13:54:24 +0530

    Added s3 bucket policy features and uploading files

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,5 +1,7 @@
 import boto3
 import click
+from pathlib import Path
+import mimetypes
 
 session = boto3.Session(profile_name = 'pythonAutomation')
 s3 = session.resource('s3')
@@ -16,11 +18,13 @@ def list_buckets():
 		print(bucket.name)
 
 @cli.command('list-bucket-objects')
-def list_bucket_objects():
+@click.argument('bucket')
+def list_bucket_objects(bucket):
 	"List all objects in a bucket"
-	for bucket in s3.buckets.all():
-		for obj in bucket.objects.all():
-			print('{0} bucket contains {1}'.format(obj.bucket_name,obj.key))
+	b = s3.Bucket(bucket)
+	print('{0} bucket contains: '.format(b.name))
+	for obj in b.objects.all():
+		print(obj.key)
 
 
 @cli.command('configure-s3-bucket')
@@ -59,8 +63,24 @@ def configure_s3_bucket(bucket):
         }})
 	return
 
+def upload_file(path,key,bucket):
+	content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+	bucket.upload_file(path,key,
+				ExtraArgs={
+				'ContentType' : content_type})
 
-
+@cli.command('sync')
+@click.argument('pathname',type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname,bucket):
+	"Syncs contents of PATHNAME to BUCKET"
+	s3_bucket = s3.Bucket(bucket)
+	root = Path(pathname).expanduser().resolve()
+	def handle_directory(target):
+	    for path in target.iterdir():
+	        if path.is_dir(): handle_directory(path)
+	        if path.is_file(): upload_file(str(path),str(path.relative_to(root)),s3_bucket)
+	handle_directory(root)
 
 if __name__ == "__main__":
 	cli()
--- README.md
+++ README.md
@@ -7,3 +7,6 @@ A script that will start a session with s3 resource
 Currently has following features:
 1. List buckets in s3
 2. List all objects in a bucket
+3. Apply a policy to bucket and create a bucket
+4. Upload files to s3 bucket
+
