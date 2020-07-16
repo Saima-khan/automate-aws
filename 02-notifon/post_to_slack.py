16adfeb90d1786e99530988788ae05f767fb2549
saima <saima.khan@geminisolutions.in>
2020-07-16 21:19:54 +0530

    Adding new files

--- 02-notifon/post_to_slack.py
+++ 02-notifon/post_to_slack.py
@@ -1,5 +1,5 @@
 # coding: utf-8
 import requests
-url = '' #Replace with slack webhook
-data = {"text":"Hello, It's Saima."}
+url = '' # Replace with slack webhook URL
+data = { "text": "Hello, world." }
 requests.post(url, json=data)
