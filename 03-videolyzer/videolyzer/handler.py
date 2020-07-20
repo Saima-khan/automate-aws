e0436c997ce6ce36133b3fc62836052bfa479c3b
saima <saima.khan@geminisolutions.in>
2020-07-20 16:51:45 +0530

    Completed videoanalyzer project

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -17,9 +17,6 @@ def start_label_detection(bucket,key):
     }
 )
 
-
-
-    print(response)
     return
 
 def get_video_labels(job_id):
@@ -39,9 +36,34 @@ def get_video_labels(job_id):
 
     return response
 
+def make_item(data):
+    if isinstance(data, dict):
+        return { k: make_item(v) for k, v in data.items() }
 
-def put_labels_in_db(data,videoname,videobucket):
-    pass
+    if isinstance(data, list):
+        return [ make_item(v) for v in data ]
+
+    if isinstance(data, float):
+        return str(data)
+
+    return data
+
+def put_labels_in_db(data,video_name,video_bucket):
+    del data['JobStatus']
+    del data['ResponseMetadata']
+    data['videoName'] = video_name
+    data['videoBucket'] = video_bucket
+
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMODB_TABLE_NAME']
+    video_table = dynamodb.Table(table_name)
+
+    data = make_item(data)
+
+    video_table.put_item(Item=data)
+
+    return
+
 
 #Lambda Events
 def start_processing_video(event,context):
@@ -61,7 +83,6 @@ def handle_label_detection(event,context):
         s3_bucket = message['Video']['S3Bucket']
 
         response = get_video_labels(job_id)
-        print(response)
         put_labels_in_db(response, s3_object, s3_bucket)
 
     return
